8cd82da1e880eda409663f4f4e8deea4f3524362
Annaruth McBride <armcbride17@gmail.com>
2020-04-16 23:57:26 -0500

    fixed server issue

--- public/server.js
+++ public/server.js
@@ -10,10 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../index.html"));
+    res.sendFile(path.join(__dirname, "./index.html"));
   });
   app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../notes.html"));
+    res.sendFile(path.join(__dirname, "./notes.html"));
   });
  
 
