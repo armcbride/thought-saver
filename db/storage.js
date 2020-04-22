8d0a49c287aa16dfe3b472a68e11b32d80983bda
Annaruth McBride <armcbride17@gmail.com>
2020-04-21 23:56:37 -0500

    refactored, adjusted test notes

--- db/storage.js
+++ db/storage.js
@@ -31,23 +31,17 @@ class Storage {
 
     addNotes(notes) {
         const {title, text} = notes;
-
         if (!title || !text){
             throw new Err ("fields cannot be blank.")
         }
         const newNote = {
             title, text, id: uuidv4()
         }
-
-        return this.getNotes().then(note => [...note, newNote])
-        .then(updatedNotes => this.write(updatedNotes))
-        .then(()=>newNote)
-
+        return this.getNotes().then(note => [...note, newNote]).then(updatedNotes => this.write(updatedNotes)).then(()=>newNote)
     }
 
     removeNote(id) {
         return this.getNotes().then(notes=>notes.filter(note => note.id !== id)).then(filteredNotes =>this.write(filteredNotes))
-
     }
 }
 
